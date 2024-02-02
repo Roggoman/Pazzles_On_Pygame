85d4108fcca4579a99763990589242c1a1ad0a79
Egor Yakovlev <egorsambo0405@mail.ru>
2024-02-02 09:22:59 +0300

    Итог

--- game.py
+++ game.py
@@ -3,45 +3,18 @@ import random
 import time
 
 
-def main(number, num):
-    start_time = time.time()
-    pygame.init()
-    WINDOW_WIDTH = 800
-    WINDOW_HEIGHT = 550
-    X = WINDOW_WIDTH
-    Y = WINDOW_HEIGHT
-    play_flag = 1
-    screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    pygame.display.set_caption('Puzzle Game')
-    FPS = 30
-    clock = pygame.time.Clock()
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    RED = (255, 0, 0)
-    CRIMSON = (220, 20, 60)
-    ORANGE = (255, 127, 0)
-    font_title = pygame.font.Font(None, 64)
-    font_content = pygame.font.Font(None, 40)
-    selected_img = None
-    is_game_over = False
-    all_time = None
-    rows = None
-    cols = None
-    cell_width = None
-    cell_height = None
-    cell_wh = list()
-    cells = list()
-    pause_im = pygame.image.load("data/pause.png")
-    play_im = pygame.image.load("data/play.png")
-    delta_time = 0
-    play_again_text = font_title.render('You WIN!', True, WHITE)
-    play_again_rect = play_again_text.get_rect()
-    play_again_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
-    continue_text = font_content.render('Press Escape', True, WHITE)
-    continue_rect = continue_text.get_rect()
-    continue_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 50)
+class Game():
+    def __init__(self, number, num):
+        self.number = number
+        self.num = num
+        self.WHITE = (255, 255, 255)
+        self.BLACK = (0, 0, 0)
+        self.RED = (255, 0, 0)
+        self.CRIMSON = (220, 20, 60)
+        self.ORANGE = (255, 127, 0)
+        self.volume = 0.5
 
-    def start_game(mode):
+    def start_game(self, mode):
         global cell_width, cell_height
         rows = mode
         cols = mode
@@ -49,99 +22,160 @@ def main(number, num):
         cell_width = 800 // rows
         cell_height = 450 // cols
         rand_indexes = list(range(0, num_cells))
-        cell_wh.append(cell_width)
-        cell_wh.append(cell_height)
+        self.cell_wh.append(cell_width)
+        self.cell_wh.append(cell_height)
         for i in range(num_cells):
             x = (i % rows) * cell_width
             y = (i // cols) * cell_height
             rect = pygame.Rect(x, y, cell_width, cell_height)
             rand_pos = random.choice(rand_indexes)
             rand_indexes.remove(rand_pos)
-            cells.append({'rect': rect, 'border': WHITE,
-                          'order': i, 'pos': rand_pos})
+            self.cells.append({'rect': rect, 'border': self.WHITE,
+                               'order': i, 'pos': rand_pos})
 
-    bg = pygame.image.load(number)
-    bg_rect = bg.get_rect()
-    bg_rect.topleft = (0, 0)
-    running = True
-    start_game(num)
-    while running:
-        screen.fill(CRIMSON)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
+    def main(self):
+        pygame.mixer.init()
+        pygame.mixer.music.load('data/music.mp3')
+        pygame.mixer.music.set_volume(self.volume)
+        pygame.mixer.music.play(loops=-1)
+        start_time = time.time()
+        pygame.init()
+        WINDOW_WIDTH = 800
+        WINDOW_HEIGHT = 550
+        X = WINDOW_WIDTH
+        Y = WINDOW_HEIGHT
+        play_flag = 1
+        screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption('Puzzle Game')
+        FPS = 30
+        clock = pygame.time.Clock()
+        font_title = pygame.font.Font(None, 64)
+        font_content = pygame.font.Font(None, 40)
+        selected_img = None
+        is_game_over = False
+        all_time = None
+        rows = None
+        cols = None
+        cell_width = None
+        cell_height = None
+        self.cell_wh = list()
+        self.cells = list()
+        pause_im = pygame.image.load("data/imgs/pause.png")
+        play_im = pygame.image.load("data/imgs/play.png")
+        delta_time = 0
+        play_again_text = font_title.render('You WIN!', True, self.WHITE)
+        play_again_rect = play_again_text.get_rect()
+        play_again_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+        continue_text = font_content.render('Press Escape', True, self.WHITE)
+        continue_rect = continue_text.get_rect()
+        continue_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2 + 50)
+
+        bg = pygame.image.load(self.number)
+        bg_rect = bg.get_rect()
+        bg_rect.topleft = (0, 0)
+        running = True
+        self.start_game(self.num)
+        while running:
+            screen.fill(self.CRIMSON)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.mixer.music.stop()
                     running = False
-                    if is_game_over:
-                        return 1
-                    else:
-                        return 0
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not is_game_over:
-                mouse_pos = pygame.mouse.get_pos()
-                pos = mouse_pos
-                if X - 50 < pos[0] < X and Y > pos[1] > Y - 50:
-                    play_flag += 1
-                    play_flag %= 2
-                for cell in cells:
-                    rect = cell['rect']
-                    order = cell['order']
-                    if rect.collidepoint(mouse_pos):
-                        if not selected_img:
-                            selected_img = cell
-                            cell['border'] = RED
+                    return 0
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        self.volume += 0.1
+                        if self.volume > 1:
+                            self.volume = 1
+                        pygame.mixer.music.set_self.volume(self.volume)
+                    if event.key == pygame.K_DOWN:
+                        self.volume -= 0.1
+                        if self.volume < 0:
+                            self.volume = 0
+                        pygame.mixer.music.set_self.volume(self.volume)
+                    if event.key == pygame.K_SPACE:
+                        play_flag += 1
+                        play_flag %= 2
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.mixer.music.stop()
+                        running = False
+                        if is_game_over:
+                            return 1
                         else:
-                            current_img = cell
-                            if current_img['order'] != selected_img['order']:
-                                # swap images
-                                temp = selected_img['pos']
-                                cells[selected_img['order']
-                                      ]['pos'] = cells[current_img['order']]['pos']
-                                cells[current_img['order']]['pos'] = temp
-                                cells[selected_img['order']]['border'] = WHITE
-                                selected_img = None
-                                # check if puzzle is solved
-                                is_game_over = True
-                                for cell in cells:
-                                    if cell['order'] != cell['pos']:
-                                        is_game_over = False
-        else:
-            if play_flag:
-                screen.blit(pause_im, (X - 50, Y - 50))
+                            return 0
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and not is_game_over:
+                    mouse_pos = pygame.mouse.get_pos()
+                    pos = mouse_pos
+                    if X - 50 < pos[0] < X and Y > pos[1] > Y - 50:
+                        play_flag += 1
+                        play_flag %= 2
+                    if play_flag:
+                        for cell in self.cells:
+                            rect = cell['rect']
+                            order = cell['order']
+                            if rect.collidepoint(mouse_pos):
+                                if not selected_img:
+                                    selected_img = cell
+                                    cell['border'] = self.RED
+                                else:
+                                    current_img = cell
+                                    if current_img['order'] != selected_img['order']:
+                                        # swap images
+                                        temp = selected_img['pos']
+                                        self.cells[selected_img['order']
+                                                   ]['pos'] = self.cells[current_img['order']]['pos']
+                                        self.cells[current_img['order']
+                                                   ]['pos'] = temp
+                                        self.cells[selected_img['order']
+                                                   ]['border'] = self.WHITE
+                                        selected_img = None
+                                        # check if puzzle is solved
+                                        is_game_over = True
+                                        for cell in self.cells:
+                                            if cell['order'] != cell['pos']:
+                                                is_game_over = False
             else:
-                screen.blit(play_im, (X - 50, Y - 50))
-            if not is_game_over:
-                for i, val in enumerate(cells):
-                    pos = cells[i]['pos']
-                    img_area = pygame.Rect(
-                        cells[pos]['rect'].x, cells[pos]['rect'].y, cell_wh[0], cell_wh[1])
-                    screen.blit(bg, cells[i]['rect'], img_area)
-                    pygame.draw.rect(
-                        screen, cells[i]['border'], cells[i]['rect'], 1)
-
                 if play_flag:
-                    finish_time = time.time()
-                    all_time = finish_time - start_time + delta_time
-                    font = pygame.font.Font(None, 90)
-                    text = font.render(
-                        str(round(all_time)), True, WHITE)
-                    screen.blit(text, (400, 475))
+                    screen.blit(pause_im, (X - 50, Y - 50))
                 else:
-                    delta_time = all_time
-                    start_time = time.time()
-                    text = font.render(
-                        str(round(delta_time)), True, WHITE)
+                    screen.blit(play_im, (X - 50, Y - 50))
+                if not is_game_over:
+                    for i, val in enumerate(self.cells):
+                        pos = self.cells[i]['pos']
+                        img_area = pygame.Rect(
+                            self.cells[pos]['rect'].x, self.cells[pos]['rect'].y, self.cell_wh[0], self.cell_wh[1])
+                        screen.blit(bg, self.cells[i]['rect'], img_area)
+                        pygame.draw.rect(
+                            screen, self.cells[i]['border'], self.cells[i]['rect'], 1)
+
+                    if play_flag:
+                        pygame.mixer.music.unpause()
+                        finish_time = time.time()
+                        all_time = finish_time - start_time + delta_time
+                        font = pygame.font.Font(None, 90)
+                        text = font.render(
+                            str(round(all_time)), True, self.WHITE)
+                        screen.blit(text, (400, 475))
+                    else:
+                        pygame.mixer.music.pause()
+                        delta_time = all_time
+                        start_time = time.time()
+                        text = font.render(
+                            str(round(delta_time)), True, self.WHITE)
+                        screen.blit(text, (400, 475))
+                else:
+                    pygame.mixer.music.pause()
+                    font = pygame.font.Font(None, 90)
+                    text = font.render(str(round(all_time)), True, self.WHITE)
                     screen.blit(text, (400, 475))
-            else:
-                font = pygame.font.Font(None, 90)
-                text = font.render(str(round(all_time)), True, WHITE)
-                screen.blit(text, (400, 475))
-                screen.blit(bg, bg_rect)
-                screen.blit(play_again_text, play_again_rect)
-                screen.blit(continue_text, continue_rect)
-        pygame.display.update()
-        clock.tick(FPS)
+                    screen.blit(bg, bg_rect)
+                    screen.blit(play_again_text, play_again_rect)
+                    screen.blit(continue_text, continue_rect)
+            pygame.display.update()
+            clock.tick(FPS)
+        pygame.mixer.music.stop()
 
 
 if __name__ == '__main__':
-    main('data/11.jpg', 3)
+    game = Game('data/imgs/11.jpg', 3)
+    game.main()
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pygame
 import game
+import pyperclip
+from PIL import Image
 from math import *
 import json
 import os
+import shutil
+import tkinter as tk
+from tkinter import messagebox
 
 pygame.init()
 
@@ -17,21 +22,27 @@ ORANGE = (238, 159, 34)
 # Флаги
 game_run_flag = True
 im_flag = True
-window_flag = -1
-enter_flag = 0
+window_flag = -2
+enter_num_flag = 0
+enter_path_flag = 0
+
+# Подгрузка изображений
+zastav_im = pygame.image.load("data/imgs/first.jpg")
+fone_im = pygame.image.load("data/imgs/fone.png")
+difficulty_im = pygame.image.load("data/imgs/difficulty.png")
+pers_level_im = pygame.image.load("data/imgs/pers_level.png")
+star_im = pygame.image.load("data/imgs/star.png")
+levels_im = pygame.image.load("data/imgs/levels.jpg")
+error_im = pygame.image.load("data/imgs/error.png")
+menu_im = pygame.image.load("data/imgs/menu.png")
 
 # Объявление переменных
 screen = pygame.display.set_mode([X, Y])
+loading = 0
 clock = pygame.time.Clock()
-zastav_im = pygame.image.load("data/first.jpg")
-fone_im = pygame.image.load("data/fone.png")
-pers_level_im = pygame.image.load("data/pers_level.png")
-star_im = pygame.image.load("data/star.png")
-levels_im = pygame.image.load("data/levels.jpg")
-error_im = pygame.image.load("data/error.png")
-menu_im = pygame.image.load("data/menu.png")
 difficulty = 3
-str_difficulty = None
+str_difficulty = str()
+str_level = str()
 x = int()
 y = int()
 
@@ -110,7 +121,9 @@ while game_run_flag:
             game_run_flag = False
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             im_flag = False
-            if window_flag == -1:
+            if window_flag == -2:
+                window_flag = -1
+            elif window_flag == -1:
                 window_flag = 0
             else:
                 pos = event.pos
@@ -125,7 +138,9 @@ while game_run_flag:
                 elif window_flag == 1:
                     number = check(pos)
                     if number != 0:
-                        star = game.main(f"data/{number}.jpg", difficulty)
+                        game_class = game.Game(
+                            f"data/imgs/{number}.jpg", difficulty)
+                        star = game_class.main()
                         screen = pygame.display.set_mode([X, Y])
                         if star == 1:
                             if number not in lst:
@@ -133,21 +148,25 @@ while game_run_flag:
                                 lst.append(number)
                     star = 0
                 elif window_flag == 2:
-                    pass
+                    enter_path_flag = 1
                 elif window_flag == 3:
-                    
-                    enter_flag = 1
+                    enter_num_flag = 1
                     if 163 < pos[0] < 867 and 424 < pos[1] < 551:
-                        if str_difficulty == None:
+                        if str_difficulty == str():
                             str_difficulty = "3"
                         difficulty = int(str_difficulty)
-                        if difficulty < 3:
+                        if difficulty < 3 or difficulty > 100:
                             difficulty = 3
-                        str_difficulty = None
+                        str_difficulty = str()
                         window_flag = 0
                 if str(window_flag) in "123":
                     if 0 < pos[0] < 50 and 0 < pos[1] < 50:
                         window_flag = 0
+                if str(window_flag) in "012":
+                    enter_num_flag = 0
+                if str(window_flag) in "013":
+                    enter_path_flag = 0
+                    str_level = str()
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 2:
             pass
         if event.type == pygame.KEYDOWN:
@@ -159,12 +178,82 @@ while game_run_flag:
                 pass
             if event.key == pygame.K_RIGHT:
                 pass
-            if event.key in (pygame.K_0, pygame.K_1, pygame.K_2, pygame.K_3, pygame.K_4, pygame.K_5, pygame.K_6, pygame.K_7, pygame.K_8, pygame.K_9):
-                if enter_flag:
-                    if str_difficulty == None:
-                        str_difficulty = ""
-                    str_difficulty += str(event.key - 48)
-
+            if enter_num_flag:
+                if event.key == pygame.K_RETURN:
+                    if str_difficulty == str():
+                        str_difficulty = "3"
+                    difficulty = int(str_difficulty)
+                    if difficulty < 3 or difficulty > 100:
+                        difficulty = 3
+                    str_difficulty = str()
+                    window_flag = 0
+                if 48 <= event.key <= 57:
+                    if str_difficulty == str():
+                        str_difficulty = str()
+                    temp = event.key - 48
+                    str_difficulty += str(temp)
+                if event.key == pygame.K_BACKSPACE:
+                    if len(str_difficulty) > 0:
+                        str_difficulty = str_difficulty[:-1]
+            if enter_path_flag:
+                if pygame.key.get_mods() & pygame.KMOD_CTRL:
+                    if event.key == pygame.K_v:
+                        str_level += pyperclip.paste()
+                if pygame.key.get_mods() & pygame.KMOD_SHIFT:
+                    if event.key in (54, 59):
+                        str_level += ':'
+                    if event.key == 45:
+                        str_level += '_'
+                    if event.key in (44, 46):
+                        str_level += '.'
+                    if 97 <= event.key <= 122 or 48 <= event.key <= 57:
+                        if event.key == pygame.K_v and str_level[-1 * len(pyperclip.paste()):] == pyperclip.paste():
+                            pass
+                        else:
+                            str_level += chr(event.key).upper()
+                    if event.key == 92:
+                        str_level += chr(event.key).upper()
+                else:
+                    if 97 <= event.key <= 122 or 48 <= event.key <= 57:
+                        if event.key == pygame.K_v and str_level[-1 * len(pyperclip.paste()):] == pyperclip.paste():
+                            pass
+                        else:
+                            str_level += chr(event.key)
+                    if event.key == 92:
+                        str_level += chr(event.key)
+                    if event.key in (54, 59):
+                        str_level += ':'
+                    if event.key == 45:
+                        str_level += '_'
+                    if event.key in (44, 46):
+                        str_level += '.'
+                    if event.key == pygame.K_ESCAPE:
+                        str_level = str()
+                    if event.key == pygame.K_BACKSPACE:
+                        if str(str_level):
+                            str_level = str_level[:-1]
+                    if event.key == pygame.K_RETURN:
+                        try:
+                            str_level = str_level.strip().lower()
+                            shutil.copyfile(str_level,
+                                            "data/imgs/person.png")
+                            img = Image.open("data/imgs/person.png")
+                            img = img.resize((800, 450))
+                            img.save("data/imgs/person.png")
+                            game_class = game.Game(
+                                f"data/imgs/person.png", difficulty)
+                            star = game_class.main()
+                            screen = pygame.display.set_mode([X, Y])
+                            stars += star
+                        except Exception as e:
+                            print(type(e).__name__)
+                            tk.Tk().wm_withdraw()
+                            messagebox.showerror(
+                                'Error', 'Неправильный путь до файла')
+                            window_flag = 0
+                        finally:
+                            screen = pygame.display.set_mode([X, Y])
+                            str_level = str()
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
                 pass
@@ -174,15 +263,28 @@ while game_run_flag:
                 pass
             if event.key == pygame.K_RIGHT:
                 pass
-
+    if window_flag == -1:
+        screen.fill(BLACK)
+        font = pygame.font.Font(None, 90)
+        text = font.render("Загрузка:", True, WHITE)
+        screen.blit(text, (x + 340, y + 120))
+        pygame.draw.rect(screen, WHITE, (150, 350, 700, 50), 5)
+        if loading < 700:
+            loading += 2
+            pygame.draw.rect(screen, WHITE, (150, 350, loading, 50))
+        else:
+            window_flag = 0
     if window_flag == 0:
         screen.blit(fone_im, (x - 5, y - 5))
     elif window_flag == 1:
         screen.blit(levels_im, (x, y))
     elif window_flag == 2:
-        screen.blit(error_im, (x, y))
+        screen.blit(pers_level_im, (x - 50, y - 50))
+        font = pygame.font.Font(None, 60)
+        text = font.render(str_level, True, WHITE)
+        screen.blit(text, (40, 260))
     elif window_flag == 3:
-        screen.blit(pers_level_im, (x, y))
+        screen.blit(difficulty_im, (x, y))
         font = pygame.font.Font(None, 90)
         text = font.render(str_difficulty, True, BLACK)
         screen.blit(text, (250, 258))
